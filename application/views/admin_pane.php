df819cd498b6e48e2d3a3d068096aacb5b9833eb
adigoks <atma.adhi@gmail.com>
2017-08-06 15:46:28 +0700

    update view nggo admin

--- application/views/admin_pane.php
+++ application/views/admin_pane.php
@@ -2,52 +2,89 @@
 
 	<script type="text/javascript" src="<?php echo base_url();?>asset/js/jquery-2.1.0.min.js"></script>
 	<script type="text/javascript" src="<?php echo base_url();?>asset/js/bootstrap.min.js"></script>
-	<?php echo link_tag('asset/css/bootstrap-theme.min.css'); ?>
-	<?php echo link_tag('asset/css/bootstrap.min.css'); ?>
+	<?php echo link_tag('asset/css/bootstrap-theme.css'); ?>
+	<?php echo link_tag('asset/css/bootstrap.css'); ?>
+	<?php echo link_tag('asset/css/admin-style.css'); ?>
 	
 </head>
 <body>
 <div id="topbar">
 	
 </div>
-<div id='main'>
-	<div id="sidebar">
-		<div id="sidebar-menu">
-			<ul>
-			  	<li>MENU
-			  		<ul>
-			    	<li>Tambah</li>
-			    	<li>Sesuaikan</li>
-			    	</ul>
-			    </li>
-			  	<li>POST
-			    	<ul>
-			    	<li>Tambah</li>
-			    	<li>Sesuaikan</li>
-			    	</ul>
-			  	</li>
-			  	
-			  	<li>TEMA
-			    	<ul>
-			    	<li>Tampilan</li>
-			    	<li>Layout</li>
-			    	</ul>
-			  	</li>
-			  	<li>
-			  		MEDIA
-			  	</li>
-			</ul>
+<div id='main' class="row">
+	<div id="sidebar" class="col-md-2">
+		<div id="sidebar-menu" >
+			<br>
+			<div id='menu-accordion' class="panel-group">
+			    <div class="panel panel-default">
+			      	<div class="panel-heading">
+			        	<h4 class="panel-title">
+			          		<a data-toggle="collapse" data-parent='#menu-accordion' href="#collapse1">MENU</a>
+			        	</h4>
+			      	</div>
+			    	<div id="collapse1" class="panel-collapse collapse" >
+			        	<ul class="list-group">
+			          		<li class="list-group-item">Tambah</li>
+			          		<li class="list-group-item">Sesuaikan</li>
+			          		
+			        	</ul>
+			        
+			      	</div>
+			    </div>
+			    <div class="panel panel-default">
+			      	<div class="panel-heading">
+			        	<h4 class="panel-title">
+			          		<a data-toggle="collapse" data-parent='#menu-accordion' href="#collapse2">POST</a>
+			        	</h4>
+			      	</div>
+			    	<div id="collapse2" class="panel-collapse collapse">
+			        	<ul class="list-group">
+			          		<li class="list-group-item">Tambah</li>
+			          		<li class="list-group-item">Sesuaikan</li>
+			          		
+			        	</ul>
+			        
+			      	</div>
+			    </div>
+			    <div class="panel panel-default">
+			      	<div class="panel-heading">
+			        	<h4 class="panel-title">
+			          		<a data-toggle="collapse" data-parent='#menu-accordion' href="#collapse3">TEMA</a>
+			        	</h4>
+			      	</div>
+			    	<div id="collapse3" class="panel-collapse collapse">
+			        	<ul class="list-group">
+			          		<li class="list-group-item">Tampilan</li>
+			          		<li class="list-group-item">Layout</li>
+			          		
+			        	</ul>
+			        
+			      	</div>
+			    </div>
+			    <div class="panel panel-default">
+			      	<div class="panel-heading">
+			        	<h4 class="panel-title">
+			          		<a href="#">MEDIA</a>
+			        	</h4>
+			      	</div>
+			    	
+			    </div>
+			</div>
+			
 			</br>
-			<ul>
-			  	<li>Pengaturan
+			<ul class="list-group">
+			  	<li class="list-group-item">Pengaturan
 			    </li>
-			    <li>Profil</li>
+			    <li class="list-group-item">Profil</li>
 			</ul>
 
 		</div>
 	</div>
 	<?php
+	if (isset($content)) {
 		echo $content;
+	}
+		
 	?>
 	
 </div>
