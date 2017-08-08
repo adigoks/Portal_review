b5fcc3145610039231fd43e37005ae69f5ef5eb0
adigoks <atma.adhi@gmail.com>
2017-08-08 16:59:10 +0700

    ubah post list

--- application/views/admin_post_list.php
+++ application/views/admin_post_list.php
@@ -1,62 +1,64 @@
 
-<div class='input-group input-group-lg'>
-	<span class=' input-group-addon' style="border-bottom-left-radius: 0">
-	</span>
-	<!-- <span style='font-size: 16px; ' class='input-group-btn'>
-		
-			<a href="#"  style='border-radius: 0' class="btn btn-default input-group-btn">
+<div style="margin-bottom: 10px;">
+	<div class='input-group input-group-lg'>
+		<span class=' input-group-addon' style="border-bottom-left-radius: 0">
+		</span>
+		<!-- <span style='font-size: 16px; ' class='input-group-btn'>
+			
+				<a href="#"  style='border-radius: 0' class="btn btn-default input-group-btn">
+					
+				</a>
+			
+		</span> -->
+		<span style='width:90%;background-color: white ;font-size: 16px;border-bottom-right-radius: 0;' class=' input-group-addon'>JUDUL</span>
+		<!-- <span style='width:8%;font-size: 16px;' class=' input-group-btn '>
+			<a href='#'  class='btn btn-primary input-group-btn'>
 				
+				<span style ='font-size: 16px;' class='glyphicon glyphicon-comment'></span>						
 			</a>
-		
-	</span> -->
-	<span style='width:90%;background-color: white ;font-size: 16px;border-bottom-right-radius: 0;' class=' input-group-addon'>JUDUL</span>
-	<!-- <span style='width:8%;font-size: 16px;' class=' input-group-btn '>
-		<a href='#'  class='btn btn-primary input-group-btn'>
-			
-			<span style ='font-size: 16px;' class='glyphicon glyphicon-comment'></span>						
-		</a>
-		<a href='#'  class='btn btn-primary input-group-btn'>
-			
-			<span style ='font-size: 16px;' class='glyphicon glyphicon-edit'></span>						
-		</a>
-		<a href='#'  class='btn btn-primary input-group-btn'>
-			
-			<span style ='font-size: 16px;' class='glyphicon glyphicon-remove'></span>						
-		</a>	
-		<a href='#'  class='btn btn-primary input-group-btn'>
-			
-			<span style ='font-size: 16px;' class='glyphicon glyphicon-new-window'></span>						
-		</a>	
-	</span> -->
-</div>
-
-<div class='input-group input-group-lg'>
-	
-	<span style='border-top-left-radius: 0;width:22%;font-size: 16px;' class='input-group-addon'>
-		
-			<!-- <a href="#"  class="btn btn-default input-group-btn" style='border-top-left-radius: 0;'>
+			<a href='#'  class='btn btn-primary input-group-btn'>
 				
-			</a> -->
+				<span style ='font-size: 16px;' class='glyphicon glyphicon-edit'></span>						
+			</a>
+			<a href='#'  class='btn btn-primary input-group-btn'>
+				
+				<span style ='font-size: 16px;' class='glyphicon glyphicon-remove'></span>						
+			</a>	
+			<a href='#'  class='btn btn-primary input-group-btn'>
+				
+				<span style ='font-size: 16px;' class='glyphicon glyphicon-new-window'></span>						
+			</a>	
+		</span> -->
+	</div>
+
+	<div class='input-group input-group-lg'>
+		
+		<span style='border-top-left-radius: 0;width:22%;font-size: 16px;' class='input-group-addon'>
 			
-			TANGGAL
-	</span>
-	<span style='width:22%;font-size: 16px;' class=' input-group-addon'>AUTHOR</span>
-	<span style='font-size: 16px;' class=' input-group-btn '>
-		<a href='#'  class='btn btn-default input-group-btn'>
-			
-			<span style ='font-size: 16px;' class='glyphicon glyphicon-comment'></span>						
-		</a>
-		<a href='#'  class='btn btn-default input-group-btn'>
-			
-			<span style ='font-size: 16px;' class='glyphicon glyphicon-edit'></span>						
-		</a>
-		<a href='#'  class='btn btn-default input-group-btn'>
-			
-			<span style ='font-size: 16px;' class='glyphicon glyphicon-remove'></span>						
-		</a>	
-		<a href='#'  class='btn btn-default input-group-btn' style="border-top-right-radius: 0;">
-			
-			<span style ='font-size: 16px;' class='glyphicon glyphicon-new-window'></span>						
-		</a>	
-	</span>
+				<!-- <a href="#"  class="btn btn-default input-group-btn" style='border-top-left-radius: 0;'>
+					
+				</a> -->
+				
+				TANGGAL
+		</span>
+		<span style='width:22%;font-size: 16px;' class=' input-group-addon'>AUTHOR</span>
+		<span style='font-size: 16px;' class=' input-group-btn '>
+			<a href='#'  class='btn btn-default input-group-btn'>
+				
+				<span style ='font-size: 16px;' class='glyphicon glyphicon-comment'></span>						
+			</a>
+			<a href='#'  class='btn btn-default input-group-btn'>
+				
+				<span style ='font-size: 16px;' class='glyphicon glyphicon-edit'></span>						
+			</a>
+			<a href='#'  class='btn btn-default input-group-btn'>
+				
+				<span style ='font-size: 16px;' class='glyphicon glyphicon-remove'></span>						
+			</a>	
+			<a href='#'  class='btn btn-default input-group-btn' style="border-top-right-radius: 0;">
+				
+				<span style ='font-size: 16px;' class='glyphicon glyphicon-new-window'></span>						
+			</a>	
+		</span>
+</div>
 </div>
