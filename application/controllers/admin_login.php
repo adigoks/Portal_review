00561af942804cc1904be23d4d9dd020425a3135
DESKTOP-S0I3TA4\Meveriz <fichasa7@gmail.com>
2017-08-15 12:58:00 +0700

    menu profil fix and logout

--- application/controllers/admin_login.php
+++ application/controllers/admin_login.php
@@ -81,7 +81,6 @@
 				redirect(site_url('admin-dashboard/menu'));
 			}
 		}
-
 	}
 
 ?>
--- application/controllers/admin_profil.php
+++ application/controllers/admin_profil.php
@@ -13,6 +13,7 @@
 			$this->load->library('session');
 			$this->load->helper('form');
 			$this->load->helper('html');
+			$this->load->model('user_model');
 			if ($this->session->userdata('logged_in') == FALSE) 
 			{
 				redirect(site_url('admin_login'));
@@ -26,10 +27,23 @@
 
 		function profil()
 		{
-			$data['content'] = $this->load->view('admin_profil_form', '', true);
+			$id = $this->session->userdata('id_author');
+			$data['user'] = $this->user_model->selectId($id)->row();
+			$data['content'] = $this->load->view('admin_profil_form', $data, true);
 
 			$data['content'] =$this->load->view('admin_body', $data,true);
 			$this->load->view('admin_pane', $data);
+		}
+
+		function update_profil()
+		{
+			$data['user_name'] = $this->input->post('username');
+			$data['user_email'] = $this->input->post('email');
+			$data['user_deskripsi'] = $this->input->post('deskripsi');
+			$id = $this->input->post('id');
+
+			$this->user_model->update($data,$id);
+			redirect(site_url('admin_profil'));
 		}
 
 	}
