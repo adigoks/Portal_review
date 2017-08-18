a59569368eefb6cc31202caab9ab28c82e15903f
joyfree <coldfaithsim@gmail.com>
2017-08-18 16:18:42 +0700

    front post tag

--- application/controllers/post.php
+++ application/controllers/post.php
@@ -9,13 +9,37 @@
 		function __construct()
 		{
 			# code...
-			parent::__construct()
+			parent::__construct();
 			$this->load->helper('url');
 			$this->load->model('post_model');
+			$this->load->helper('url');
+			$this->load->library('session');
+			$this->load->helper('html');
+			$this->load->model('menu_model');
+		}
+		public function index()
+		{
+			$this->load->view('front_head');
+			$this->menu_list();
+			$this->tag();
+			$this->load->view('front_footer');
+		}
+		public function menu_list()
+		{
+			$data['menu'] = $this->menu_model->selectSort();
+			$this->load->view('front_header', $data);
 		}
 
 		public function tag()
 		{
+			
+			$data['latest_main'] =$this->load->view('front_post','',true);
+
+			$data['content'] = $this->load->view('front_main_post', $data,true);
+
+			// $data['content'] .=  $this->load->view('front_content',$data,true);
+			$this->load->view('front_body', $data);
+			
 			# code...
 		}
 
