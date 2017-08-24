2f7d54bf67b18c2cc742591dd6e82c8e8d8fe745
joyfree <coldfaithsim@gmail.com>
2017-08-24 16:45:54 +0700

    front trending

---
+++ application/views/front_trending.php
@@ -0,0 +1,23 @@
+<div class="list-group">
+	<h2 class="tag-wid">
+		<span class="bord ini-tag-judul">TRENDING</span>
+	</h2>
+	<div class="top-d pb">
+			<div class="number"><h2></h2></div>
+			<div class="pop-list-title"><a href=""><h4><b>blablabla</b></h5></a></div>
+	</div>
+</div>	
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		if(window.innerWidth < 500){
+			  $('#img').hide();	
+		}
+		else {
+			$('#img').show();
+		}
+		
+		
+	});
+</script>
+	
