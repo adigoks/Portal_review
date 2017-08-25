aa37f82d8563d8fa67fe9161f1f602c91848ec5e
DESKTOP-S0I3TA4\Meveriz <fichasa7@gmail.com>
2017-08-25 14:48:58 +0700

    profil fix(belum test full), session bug fix, add trending

--- application/views/front_trending.php
+++ application/views/front_trending.php
@@ -3,8 +3,16 @@
 		<span class="bord ini-tag-judul">TRENDING</span>
 	</h2>
 	<div class="top-d pb">
-			<div class="number"><h2></h2></div>
-			<div class="pop-list-title"><a href=""><h4><b>blablabla</b></h5></a></div>
+	<?php $i=0; foreach ($trending as $t) { 
+
+		if ($i < 5) { 
+			$i++; ?>
+
+			<div class="number"><h2><?php echo $i; ?>.</h2></div>
+			<div class="pop-list-title"><a href="<?php echo base_url().'post/'.$t->post_uri; ?>"><h4><b><?php echo $t->post_judul; ?></b></h5></a></div>
+
+			<?php } 
+		} ?>
 	</div>
 </div>	
 
