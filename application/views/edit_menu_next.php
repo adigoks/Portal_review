95d769531f7ca01faf7668d9b5b69fc91728b913
joyfree <coldfaithsim@gmail.com>
2017-08-27 20:23:47 +0700

    fixing edit menu

--- application/views/edit_menu_next.php
+++ application/views/edit_menu_next.php
@@ -2,28 +2,33 @@
 	<h2>Edit Menu</h2>
 
 	<fieldset>
-		<form action="<?php echo site_url('admin_menu/edit_menu'); ?>" method="POST">
-			
-			Nama Menu : 
-			<input type="text" name="menu_name" value="<?php echo $name ?>" readonly="readonly" /><br/><br/>
-			Tipe Menu : 
-			<input type="text" name="menu_tipe" value="<?php echo $tipe ?>" readonly="readonly" /><br/><br/>
-			
+		<form class="col-md-4 form-horizontal" action="<?php echo site_url('admin_menu/edit_menu'); ?>" method="POST">
+			<div class="form-group">
+				<label for="menu_name">Nama Menu :</label> 
+				<input class="form-control " type="text" name="menu_name" value="<?php echo $name ?>" readonly="readonly" />
+			</div>
+			<div class="form-group">
+				<label for="tipe_name">Tipe Menu :</label> 
+				<input class="form-control " type="text" name="menu_tipe"  value="<?php echo $tipe ?>" readonly="readonly" />
+			</div>	
 
 			<?php if ($tipe == 'external_link'): ?>
-				External Link :
-			<input type="text" name="url" value="<?php if ($url_name->menu_url_type == 'external_link') {
+			<div class="form-group">
+				<label for="url">External Link :</label> 
+				<input class="form-control " type="text" name="url"  value="<?php if ($url_name->menu_url_type == 'external_link') {
 				echo $url_name->menu_url;
 			} else {
 				echo "" ;
 			}
-			 ?>" /><br/><br/>
+			 ?>" />
+			</div>	
 			<?php endif ?>
 
 
 			<?php if ($tipe == 'page'): ?>
-				Judul Page :
-			<select name="url">
+			<div class="form-group">
+				<label for="url">Judul Page :</label>
+				<select class="form-control" name="url">
 				<option value="<?php echo $url_name->menu_url; ?>"><?php if ($url_name->menu_url_type == 'page') {
 					echo $url_name->menu_url;
 				} else {
@@ -35,47 +40,54 @@
 				{ ?>
 					<option value="<?php echo $page->page_judul; ?>"><?php echo $page->page_judul; ?></option>
 				<?php } ?>
-			</select><br/><br/>
+			</select> 
+
+			</div>
 			<?php endif ?>
 
 
 			<?php if ($tipe == 'post'): ?>
-				Judul Post :
-			<select name="url">
-				<option value="<?php echo $url_name->menu_url; ?>"><?php if ($url_name->menu_url_type == 'post') {
-					echo $url_name->menu_url;
-				} else {
-					echo "" ;
-				}
-				 ?></option>
-				<option></option>
-				<?php foreach ($post_list as $post) 
-				{ ?>
-					<option value="<?php echo $post->post_judul; ?>"><?php echo $post->post_judul; ?></option>
-				<?php } ?>
-			</select><br/><br/>
+			<div class="form-group">
+				<label for="url">Judul Post :</label> 
+				<select class="form-control" name="url">
+					<option value="<?php echo $url_name->menu_url; ?>"><?php if ($url_name->menu_url_type == 'post') {
+						echo $url_name->menu_url;
+					} else {
+						echo "" ;
+					}
+					 ?></option>
+					<option></option>
+					<?php foreach ($post_list as $post) 
+					{ ?>
+						<option value="<?php echo $post->post_judul; ?>"><?php echo $post->post_judul; ?></option>
+					<?php } ?>
+				</select>
+			</div>
 			<?php endif ?>
 
 
 			<?php if ($tipe == 'tag'): ?>
-				Nama Tag :
-			<select name="url">
-				<option value="<?php echo $url_name->menu_url; ?>"><?php if ($url_name->menu_url_type == 'tag') {
-					echo $url_name->menu_url;
-				} else {
-					echo "" ;
-				}
-				 ?></option>
-				<option></option>
-				<?php foreach ($post_list as $post) 
-				{ ?>
-					<option value="<?php echo $post->post_tag; ?>"><?php echo $post->post_tag; ?></option>
-				<?php } ?>
-			</select><br/><br/>
+			<div class="form-group">
+				<label for="url">Nama Tag :</label> 
+				<select class="form-control" name="url">
+					<option value="<?php echo $url_name->menu_url; ?>"><?php if ($url_name->menu_url_type == 'tag') {
+						echo $url_name->menu_url;
+					} else {
+						echo "" ;
+					}
+					 ?></option>
+					<option></option>
+					<?php foreach ($post_list as $post) 
+					{ ?>
+						<option value="<?php echo $post->post_tag; ?>"><?php echo $post->post_tag; ?></option>
+					<?php } ?>
+				</select>	
+			</div>
 			<?php endif ?>
 
 			<input type="hidden" name="id" value="<?php echo $url_name->id ?>">
-			<input type="submit" name="submit" value="Submit" />
+			<input class="btn btn-default" type="submit" name="submit" value="Submit" />
 		</form>
 
 	</fieldset>
+</div>
