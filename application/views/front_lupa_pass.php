78995dfd860aa787d113c6da088c70ea29125ffb
joyfree <coldfaithsim@gmail.com>
2017-08-28 14:22:32 +0700

    fix front comment

---
+++ application/views/front_lupa_pass.php
@@ -0,0 +1,18 @@
+<div class="form-log-u act">
+	<div class="font-log container-fluid bg">
+	<h1 class="t-log">LUPA PASSWORD</h1><br>
+	
+	<div class="form-log form-bord col-md-4 col-md-offset-1">
+	<fieldset>
+		<div style="padding-bottom: 30px;">
+		<span class="lp-alert alert alert-warning">Silahkan masukan e-mail yang sudah terdaftar!</span></br>
+		</div>
+		<form class="form-horizontal col-md-12">
+			<input class="form-control" type="e-mail" name="e-mail" placeholder="e-mail"></br>	
+			<input style="" class="btn btn-default" type="submit" name="lupa" value="kirim">
+			<input class="btn btn-primary" type="submit" name="lupa" value="cancel">
+		</form>
+	</fieldset>
+	</div>
+	</div>
+</div>
